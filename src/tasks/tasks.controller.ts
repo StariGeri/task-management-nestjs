5a4b01193bb5a10e81c4ad112169ca5248757c24
Starosta Gergő Bence <sgergostarosta@gmail.com>
2024-11-09 20:27:46 +0100

    Filtering tasks

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -6,18 +6,27 @@ import {
   Param,
   Patch,
   Post,
+  Query,
 } from '@nestjs/common';
 import { TasksService } from './tasks.service';
 import { Task, TaskStatus } from './task.model';
 import { CreateTaskDto } from './dto/create-task.dto';
+import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 
 @Controller('tasks')
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
   @Get()
-  getAllTasks(): Task[] {
-    return this.tasksService.getAllTasks();
+  getTasks(@Query() filterDto: GetTasksFilterDto): Task[] {
+    // if we have any filters defined, call tasksSevice.getTasksWithFilters
+    if (Object.keys(filterDto).length) {
+      return this.tasksService.getTasksWithFilters(filterDto);
+    } else {
+      // othervise, get all tasks
+
+      return this.tasksService.getAllTasks();
+    }
   }
 
   @Get('/:id') // path parameter, to distinct the 2 get methods
