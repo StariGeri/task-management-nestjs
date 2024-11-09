c2542dde27b36e441667a55d14c9caa88b110a87
Starosta Gergő Bence <sgergostarosta@gmail.com>
2024-11-09 19:15:35 +0100

    first commit

---
+++ src/tasks/tasks.service.ts
@@ -0,0 +1,46 @@
+import { Injectable } from '@nestjs/common';
+import { Task, TaskStatus } from './task.model';
+import { v4 as uuid } from 'uuid';
+import { CreateTaskDto } from './dto/create-task.dto';
+
+@Injectable()
+export class TasksService {
+  private tasks: Task[] = [];
+
+  getAllTasks(): Task[] {
+    return this.tasks;
+  }
+
+  getTaskById(id: string): Task {
+    return this.tasks.find((task) => task.id === id);
+  }
+
+  createTask(createTaskDto: CreateTaskDto): Task {
+    const { title, description } = createTaskDto;
+
+    const task: Task = {
+      id: uuid(),
+      title: title,
+      description: description,
+      status: TaskStatus.OPEN,
+    };
+
+    this.tasks.push(task);
+
+    console.log('tasks', this.tasks);
+
+    return task;
+  }
+
+  deleteTask(id: string): void {
+    this.tasks = this.tasks.filter((task) => task.id !== id);
+  }
+
+  updateTaskStatus(id: string, status: TaskStatus): Task {
+    const taskToUpdate = this.getTaskById(id);
+
+    taskToUpdate.status = status;
+
+    return taskToUpdate;
+  }
+}
